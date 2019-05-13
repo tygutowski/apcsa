9246d86386c5f2c824c80f0264d9d3251336c814
Ty Gutowski <tygutowski@gmail.com>
2019-05-13 13:42:02 -0400

    Add files via upload

--- PRB006.java
+++ PRB006.java
@@ -1,44 +1,49 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class PRB006 {
+	static String total = "";
+	static int quarters = 0; static int dimes = 0; static int nickles = 0; static int pennies = 0;
+	static ArrayList<Float> money = new ArrayList<Float>();
 	public static void main(String[] args) {
-		int num = 1;
-		Scanner input = new Scanner(System.in);
-		System.out.print("How many times? ");
-		String tempnum = input.nextLine();
-		num = Integer.parseInt(tempnum);
-		int quarters = 0; int dimes = 0; int nickles = 0; int pennies = 0;
-		String total = "";
-		for(int i = num; i>0; i--){	
-	
-			System.out.print("");
-			total += input.nextLine();
-			
+		getTotal();
+		getChange();
+	}
+		
+		public static void getTotal(){
+			int num = 1;
+			Scanner input = new Scanner(System.in);
+			System.out.print("How many times? ");
+			String tempnum = input.nextLine();
+			num = Integer.parseInt(tempnum);
+			for(int i = num; i>0; i--){	
+				System.out.print("");
+				String temp = input.nextLine();
+				temp = (temp.substring(1,temp.length()));
+				float cash = Float.parseFloat(temp);
+				money.add(cash);
+			}
 		}
-			
-
-		for(int z = 0; z<total.length(); z+=5){
-			String temp = total.substring(z+1, z+5);
-			
-			float money = Float.parseFloat(temp);
-			int cash = (int) (money * 100);
-			
-			quarters = cash/25;
-			cash %= 25;
-			dimes = cash/10;
-			cash %= 10;
-			nickles = cash/5;
-			cash %= 5;
-			pennies = cash;
-			
-			System.out.println(
-					"$" + money + "\n" + 
-					"Quarters= " + (int) quarters + "\n" +
-					"Dimes= " + (int) dimes + "\n" +
-		     		"Nickles= " + (int) nickles + "\n" +
-					"Pennies= " + (int) pennies + "\n"
-					);
+		public static void getChange(){
+			for(int z = 0; z < money.size(); z+=1){
+				int cash = (int) (money.get(z) * 100);
+				
+				quarters = cash/25;
+				cash %= 25;
+				dimes = cash/10;
+				cash %= 10;
+				nickles = cash/5;
+				cash %= 5;
+				pennies = cash;
+				
+				System.out.println(
+						"$" + money.get(z) + "\n" + 
+						"Quarters= " + (int) quarters + "\n" +
+						"Dimes= " + (int) dimes + "\n" +
+			     		"Nickles= " + (int) nickles + "\n" +
+						"Pennies= " + (int) pennies + "\n"
+						);
 		}
 	}
 }
