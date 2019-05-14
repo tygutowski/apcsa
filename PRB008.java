44dba71def59ebe54dd296983cf22b907d31eb16
Ty Gutowski <tygutowski@gmail.com>
2019-05-14 13:16:12 -0400

    Add files via upload

---
+++ PRB008.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class PRB008 {
+	static String total = "";
+	static int quarters = 0; static int dimes = 0; static int nickles = 0; static int pennies = 0;
+	static ArrayList<Float> money = new ArrayList<Float>();
+	public static void main(String[] args) {
+		getStudentGPA();
+		hasHighestGPA();
+	}
+		
+		public static void getStudentGPA(){
+			
+		}
+		public static void hasHighestGPA(){
+		
+		}
+	}
